d94ea59cc3dff47c7943d40303fabccbfe363288
BuildTools <ronlvn111@gmail.com>
2026-04-09 02:52:28 +0300

    added ArrayGenLogic and moved there some funcs from Form1

---
+++ sort algo visual/ArrayGenLogic.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Rebar;
+
+namespace sort_algo_visual
+{
+    internal static class ArrayGenLogic
+    {
+        public static Bar[] currentArray;
+        public static List<Bar[]> frames;
+        public static int currentFrame = 0;
+        public static int size = 50;
+        private static Random rand = new Random();
+
+
+        public static void GenerateNewRandomArray()
+        {
+            currentArray = new Bar[size];
+            for (int i = 0; i < size; i++)
+            {
+                currentArray[i].Value = i + 1;
+                currentArray[i].BarColor = GetRainbowColor(i, size);
+            }
+
+            Shuffle(currentArray);
+            frames = new List<Bar[]>();
+            frames.Add((Bar[])currentArray.Clone());
+            currentFrame = 0;
+        }
+
+
+
+        public static void GenerateSortedArray()
+        {
+            currentArray = new Bar[size];
+            for (int i = 0; i < size; i++)
+            {
+                currentArray[i].Value = i + 1;
+                currentArray[i].BarColor = GetRainbowColor(i, size);
+            }
+            frames = new List<Bar[]>();
+            frames.Add((Bar[])currentArray.Clone());
+            currentFrame = 0;
+            
+        }
+
+
+
+        public static void GenerateReversedArray()
+        {
+            currentArray = new Bar[size];
+            for (int i = 0; i < size; i++)
+            {
+                currentArray[i].Value = size - i;
+                currentArray[i].BarColor = GetRainbowColor(size - i - 1, size);
+            }
+            frames = new List<Bar[]>();
+            frames.Add((Bar[])currentArray.Clone());
+            currentFrame = 0;
+        }
+
+        public static Color GetRainbowColor(int index, int total) // מחזיר צבע תלוי אינדקס וכמות איברים
+        {
+            float hue = ((float)index / total) * 360f;
+            return ColorFromHSV(hue, 0.8f, 0.9f);
+        }
+
+        public static void Shuffle(Bar[] arr)
+        {
+            for (int i = arr.Length - 1; i > 0; i--)
+            {
+                int k = rand.Next(i + 1);
+                Bar temp = arr[i];
+                arr[i] = arr[k];
+                arr[k] = temp;
+            }
+        }
+
+        public static Color ColorFromHSV(double hue, double saturation, double value)// hsv to rgb conversion (thanks gemini)
+        {
+            int hi = Convert.ToInt32(Math.Floor(hue / 60)) % 6;
+            double f = hue / 60 - Math.Floor(hue / 60);
+            value = value * 255;
+            int v = Convert.ToInt32(value);
+            int p = Convert.ToInt32(value * (1 - saturation));
+            int q = Convert.ToInt32(value * (1 - f * saturation));
+            int t = Convert.ToInt32(value * (1 - (1 - f) * saturation));
+
+            if (hi == 0) return Color.FromArgb(255, v, t, p);
+            if (hi == 1) return Color.FromArgb(255, q, v, p);
+            if (hi == 2) return Color.FromArgb(255, p, v, t);
+            if (hi == 3) return Color.FromArgb(255, p, q, v);
+            if (hi == 4) return Color.FromArgb(255, t, p, v);
+            return Color.FromArgb(255, v, p, q);
+        }
+
+    }
+}
--- sort algo visual/Form1.cs
+++ sort algo visual/Form1.cs
@@ -4,78 +4,29 @@ using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Windows.Forms;
+using static sort_algo_visual.ArrayGenLogic;
 
 namespace sort_algo_visual
 {
     public partial class Form1 : Form
     {
-        private Bar[] currentArray;
-        private List<Bar[]> frames;
-        private int currentFrame = 0;
-        private Random rand = new Random();
-        int size = 50;
+
 
         public Form1()
         {
             InitializeComponent();
             this.WindowState = FormWindowState.Maximized; // להפתח במסך מלא
             GenerateNewRandomArray();
-        }
-
-        private void GenerateNewRandomArray()
-        {
-            currentArray = new Bar[size];
-            for (int i = 0; i < size; i++){
-                currentArray[i].Value = i+1;
-                currentArray[i].BarColor = GetRainbowColor(i, size);
-            }
-
-            Shuffle(currentArray);
-            frames = new List<Bar[]>();
-            frames.Add((Bar[])currentArray.Clone());
-            currentFrame = 0;
-            pictureBox1.Invalidate();
-        }
-
-        private void GenerateSortedArray()
-        {
-            currentArray = new Bar[size];
-            for (int i = 0; i < size; i++)
-            {
-                currentArray[i].Value = i + 1;
-                currentArray[i].BarColor = GetRainbowColor(i, size);
-            }
-            frames = new List<Bar[]>();
-            frames.Add((Bar[])currentArray.Clone());
-            currentFrame = 0;
             pictureBox1.Invalidate();
         }
 
 
-        private void GenerateReversedArray()
-        {
-            currentArray = new Bar[size];
-            for (int i = 0; i < size; i++)
-            {
-                currentArray[i].Value = size - i;
-                currentArray[i].BarColor = GetRainbowColor(size - i - 1, size);
-            }
-            frames = new List<Bar[]>();
-            frames.Add((Bar[])currentArray.Clone());
-            currentFrame = 0;
-            pictureBox1.Invalidate();
-        }
-
-        private Color GetRainbowColor(int index, int total) // מחזיר צבע תלוי אינדקס וכמות איברים
-        {
-            float hue = ((float)index / total) * 360f;
-            return ColorFromHSV(hue, 0.8f, 0.9f);
-        }
 
         private void btnReset_Click(object sender, EventArgs e)
         {
             timer1.Stop();
             GenerateNeededArray();
+            pictureBox1.Invalidate();
         }
 
         private void GenerateNeededArray()
@@ -84,29 +35,36 @@ namespace sort_algo_visual
             string order = arrType.SelectedItem.ToString();
             switch (order) { 
                 case "Random":
-                    GenerateNewRandomArray();
+                    ArrayGenLogic.GenerateNewRandomArray();
+                    pictureBox1.Invalidate();
                     break;
                 case "Sorted":
-                    GenerateSortedArray();
+                    ArrayGenLogic.GenerateSortedArray();
+                    pictureBox1.Invalidate();
                     break;
                 case "Reversed":
-                    GenerateReversedArray();
+                    ArrayGenLogic.GenerateReversedArray();
+                    pictureBox1.Invalidate();
                     break;
                 case "Best Case":
                     if (cmbAlgorithm.SelectedItem == null) return;
                     string alg = cmbAlgorithm.SelectedItem.ToString();
                     switch (alg) { 
                         case "Bubble Sort":
-                            GenerateSortedArray();
+                            ArrayGenLogic.GenerateSortedArray();
+                            pictureBox1.Invalidate();
                             break;
                         case "Quick Sort":
-                            GenerateSortedArray();
+                            ArrayGenLogic.GenerateSortedArray();
+                            pictureBox1.Invalidate();
                             break;
                         case "Bogo Sort":
-                            GenerateSortedArray();
+                            ArrayGenLogic.GenerateSortedArray();
+                            pictureBox1.Invalidate();
                             break;
                         case "Merge Sort":
-                            GenerateSortedArray();
+                            ArrayGenLogic.GenerateSortedArray();
+                            pictureBox1.Invalidate();
                             break;
                     }
                     break;
@@ -117,10 +75,12 @@ namespace sort_algo_visual
                     switch (alg)
                     {
                         case "Bubble Sort":
-                            GenerateReversedArray();
+                            ArrayGenLogic.GenerateReversedArray();
+                            pictureBox1.Invalidate();
                             break;
                         case "Quick Sort":
-                            GenerateSortedArray();
+                            ArrayGenLogic.GenerateSortedArray();
+
 
                             for (int i = 0; i < size; i++){
                                 int mid = i / 2;
@@ -135,11 +95,13 @@ namespace sort_algo_visual
                             frames = new List<Bar[]>();
                             frames.Add((Bar[])currentArray.Clone());
                             currentFrame = 0;
+
                             pictureBox1.Invalidate();
 
                             break;
                         case "Bogo Sort":
-                            GenerateNewRandomArray();
+                            ArrayGenLogic.GenerateNewRandomArray();
+                            pictureBox1.Invalidate();
                             break;
                         case "Merge Sort":
                             currentArray = new Bar[size];
@@ -216,7 +178,8 @@ namespace sort_algo_visual
                 currentFrame++;
                 pictureBox1.Invalidate();
             }
-            else { timer1.Stop(); }
+            else { timer1.Stop();
+            }
         }
 
         private void pictureBox1_Paint(object sender, PaintEventArgs e)
@@ -229,7 +192,8 @@ namespace sort_algo_visual
             float gap = 5.0f;
             float barWidth = Math.Max(1, stepWidth - gap);
 
-            for (int i = 0; i < frame.Length; i++){
+            for (int i = 0; i < frame.Length; i++)
+            {
                 int barHeight = (int)(((float)frame[i].Value / frame.Length) * (pictureBox1.Height - 40));
                 float x = (i * stepWidth) + (gap / 2);
                 float y = pictureBox1.Height - barHeight;
@@ -246,39 +210,13 @@ namespace sort_algo_visual
             timer1.Interval = 101 - tbSpeed.Value;
         }
 
-        private void Shuffle(Bar[] arr)
-        {
-            for (int i = arr.Length - 1; i > 0; i--){
-                int k = rand.Next(i + 1);
-                Bar temp = arr[i];
-                arr[i] = arr[k];
-                arr[k] = temp;
-            }
-        }
-
-        public static Color ColorFromHSV(double hue, double saturation, double value)// hsv to rgb conversion (thanks gemini)
-        {
-            int hi = Convert.ToInt32(Math.Floor(hue / 60)) % 6;
-            double f = hue / 60 - Math.Floor(hue / 60);
-            value = value * 255;
-            int v = Convert.ToInt32(value);
-            int p = Convert.ToInt32(value * (1 - saturation));
-            int q = Convert.ToInt32(value * (1 - f * saturation));
-            int t = Convert.ToInt32(value * (1 - (1 - f) * saturation));
-
-            if (hi == 0) return Color.FromArgb(255, v, t, p);
-            if (hi == 1) return Color.FromArgb(255, q, v, p);
-            if (hi == 2) return Color.FromArgb(255, p, v, t);
-            if (hi == 3) return Color.FromArgb(255, p, q, v);
-            if (hi == 4) return Color.FromArgb(255, t, p, v);
-            return Color.FromArgb(255, v, p, q);
-        }
-
+        
         private void trackBar1_Scroll(object sender, EventArgs e)
         {
             size = trackBar1.Value;
             timer1.Stop();
             GenerateNeededArray();
+            pictureBox1.Invalidate();
 
         }
 
@@ -286,6 +224,7 @@ namespace sort_algo_visual
         {
             timer1.Stop(); 
             GenerateNeededArray();
+            pictureBox1.Invalidate();
         }
     }
 }
